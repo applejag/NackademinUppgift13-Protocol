9e773a654bedf6b7771e88dda90fc87f2c8ef15f
kalle (jag) <kalle.jillheden@gmail.com>
2018-12-10 11:52:16 +0100

    Added custom exceptions to be implemented

---
+++ BattleshipProtocol/Protocol/Exceptions/ProtocolFormatException.cs
@@ -0,0 +1,12 @@
+﻿using BattleshipProtocol.Protocol.Commands;
+using JetBrains.Annotations;
+
+namespace BattleshipProtocol.Protocol.Exceptions
+{
+    public class ProtocolFormatException : ProtocolException
+    {
+        public ProtocolFormatException()
+            : base(ResponseCode.SyntaxError, $"Syntax error: Unable to parse packet.")
+        { }
+    }
+}
