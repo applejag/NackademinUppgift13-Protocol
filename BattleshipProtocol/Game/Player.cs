79e9850dba20b6942f55a69ebbf80fcc17526997
kalle (jag) <kalle.jillheden@gmail.com>
2018-12-15 14:05:36 +0100

    Added event on player name change

--- BattleshipProtocol/Game/Player.cs
+++ BattleshipProtocol/Game/Player.cs
@@ -1,26 +1,40 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using JetBrains.Annotations;
 
 namespace BattleshipProtocol.Game
 {
     public class Player
     {
+        [CanBeNull] private string _name;
+
         [NotNull]
         public Board Board { get; set; }
 
         [CanBeNull]
-        public string Name { get; set; }
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnNameChanged(); }
+        }
 
         public bool IsLocal { get; }
 
         [NotNull]
         public EndPoint EndPoint { get; }
 
+        public event EventHandler NameChanged;
+
         public Player(bool isLocal, EndPoint endPoint)
         {
             IsLocal = isLocal;
             EndPoint = endPoint;
             Board = new Board();
+        }
+
+        protected virtual void OnNameChanged()
+        {
+            NameChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
